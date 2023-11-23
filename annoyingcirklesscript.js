2bd28831d4c3e646ccf36c49ab7da5bc63290a73
WinterWolfSV <what112222@gmail.com>
2023-11-23 15:10:02 +0100

    The kaoz is evolving

--- annoyingcirklesscript.js
+++ annoyingcirklesscript.js
@@ -1,19 +1,17 @@
-window.onload = function () {
-    // Get mouse position
-    var mousePos;
-    document.onmousemove = function (e) {
-        mousePos = {x: e.pageX, y: e.pageY};
-    }
-    // Run code every 10ms
-    setInterval(function () {
-        // Get mouse position
-        if (mousePos) {
-            // Create box at mouse position
-            createBox(mousePos);
-        }
-    }, 20);
-
+// Get mouse position
+var mousePos;
+document.onmousemove = function (e) {
+    mousePos = {x: e.pageX, y: e.pageY};
 }
+// Run code every 10ms
+setInterval(function () {
+    // Get mouse position
+    if (mousePos) {
+        // Create box at mouse position
+        createBox(mousePos);
+    }
+}, 20);
+
 
 function createBox(position) {
     let size = getRandomSize(10, 69);
